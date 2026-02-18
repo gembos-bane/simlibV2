c0c7e0320c16b53f361bc2599876fe26457ab775
gembos-bane <110708344+gembos-bane@users.noreply.github.com>
2026-02-18 10:41:44 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -76,9 +76,9 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => 'gembos',
-	'password' => 'gembos210779',
-	'database' => 'data_simlib',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'simlib',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
@@ -92,5 +92,6 @@ $db['default'] = array(
 	'compress' => FALSE,
 	'stricton' => FALSE,
 	'failover' => array(),
-	'save_queries' => TRUE
+	'save_queries' => TRUE,
+
 );
